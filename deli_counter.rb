ced8c36678dd81f54eea0bc7d428d14ba22f1d79
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-23 22:40:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,7 @@
 # Write your code here.
 def line(array)
-  message = "The line is currently:"
-  array.each_with_index do |person, place_in_line|
-  if array.size > 0
-    puts message += "#{place_in_line}"
-  else
+  if array.size == 0 
     puts "The line is currently empty."
-   end
   end
 end
 
