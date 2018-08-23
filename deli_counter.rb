db0c073dce7950d42c880b5aef9300a6fd612f84
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-23 22:33:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,6 @@
 # Write your code here.
 def line(array)
-  beginning = "The line is currently"
-  if array.size == 0
-    puts "The line is currently empty."
-  else 
-    array.each_with_index do |person, place_in_line|
-      puts `#{beginning} += {place_in_line + 1}. #{person}.`
-    end
-  end
+  
 end
 
 def take_a_number(current_line, person)
