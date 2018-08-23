49bb02c11e1d3a5d9748d7b4b9ded88a4722d70b
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-23 22:44:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,13 @@
 # Write your code here.
 def line(array)
+  message = "The line is currently:"
   if array.size == 0 
     puts "The line is currently empty."
+  else
+    array.each_with_index do |person, place_in_line|
+      message = person
   end
-  binding.pry
+  
 end
 
 def take_a_number(current_line, person)
@@ -11,5 +15,4 @@ def take_a_number(current_line, person)
   current_line.each_with_index do |person, place_in_line|
   puts "Welcome, #{person}. You are number #{place_in_line + 1}"
  end
- message
 end
