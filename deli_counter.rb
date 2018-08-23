82848c2f7fe8103f8c11d92583387b050e8201ef
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-23 22:34:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line(array)
 end
 
 def take_a_number(current_line, person)
+  message = "The line is currently:"
   current_line.each_with_index do |person, place_in_line|
   puts "Welcome, #{person}. You are number #{place_in_line + 1}"
  end
