652d4a0eb9d097c5ded37c2cdeabb00cf551a213
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-23 22:46:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
     puts "The line is currently empty."
   else
     array.each_with_index do |person, place_in_line|
-      message = person
+      message = "#{person}
     end
   end
   
