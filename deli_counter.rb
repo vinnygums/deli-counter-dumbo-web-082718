ac077303332b21764ee732e8dc23a7a4c76c83f7
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-23 22:35:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,4 +8,5 @@ def take_a_number(current_line, person)
   current_line.each_with_index do |person, place_in_line|
   puts "Welcome, #{person}. You are number #{place_in_line + 1}"
  end
+ message
 end
