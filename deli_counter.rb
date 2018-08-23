cdcc455ce3e3ee0015e759fab2ca5498d27818ed
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-23 22:30:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,5 +10,7 @@ def line(array)
 end
 
 def take_a_number(current_line, person)
-  puts "Welcome, #{person}. You are number current_line[]
+  current_line.each_with_index do |person, place_in_line|
+  puts "Welcome, #{person}. You are number #{place_in_line + 1}"
+ end
 end
